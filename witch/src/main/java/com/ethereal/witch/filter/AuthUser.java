b3232adc07e62260c9b53af05e3894a17cc0157e
stun106 <bladingnordeste@gmail.com>
2024-01-07 16:23:52 -0300

    Fix: refatorando o recurso de autentificação, principio de responsabilidade única.

--- witch/src/main/java/com/ethereal/witch/filter/AuthUser.java
+++ witch/src/main/java/com/ethereal/witch/filter/AuthUser.java
@@ -1,12 +1,12 @@
 package com.ethereal.witch.filter;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import com.ethereal.witch.interfaces.IUserRepository;
+import com.ethereal.witch.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,41 +14,51 @@ import java.io.IOException;
 import java.util.Base64;
 
 @Component
+@AllArgsConstructor
 public class AuthUser extends OncePerRequestFilter {
-    @Autowired
-    private IUserRepository userRepository;
+
+    private final UserService userService;
+    private byte[] getBase64Decoder(String auth){
+        return Base64.getDecoder().decode(auth);
+    }
+    private String[] getCredential(byte[] decode){
+        String authString = new String((decode));
+        return authString.split(":");
+    }
+    private Boolean decodePassword(char[] password, String userPassword){
+        return BCrypt.verifyer().verify(password,userPassword).verified;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        var servletPath = request.getServletPath();
+
+        String servletPath = request.getServletPath();
 
        if (servletPath.endsWith("/auth")){
-            var auth = request.getHeader("Authorization");
-            var authEncode = auth.substring("basic".length()).trim();
-            byte[] authDecode = Base64.getDecoder().decode(authEncode);
+            String auth = request.getHeader("Authorization");
+            String authEncode = auth.substring("basic".length()).trim();
+            byte[] authDecode = getBase64Decoder(authEncode);
 
-            var authString = new String((authDecode));
-            String[] credential = authString.split(":");
+            String[] credential = getCredential(authDecode);
 
             String username = credential[0];
             String password = credential[1];
-            System.out.println(username + "<---------");
-            var user = this.userRepository.findByUsername(username);
+            var user = userService.findUsername(username);
             if (user == null){
                 response.sendError(401);
             }else{
-                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-                System.out.println(passwordVerify + "<------------");
-                if (passwordVerify.verified){
+
+                if (decodePassword(password.toCharArray(),user.getPassword())){
                     request.setAttribute("idUser",user.getId());
                     filterChain.doFilter(request,response);
                 }else{
                     response.sendError(401);
                 }
             }
-        }else {
+
+       }else {
            filterChain.doFilter(request,response);
        }
     }
