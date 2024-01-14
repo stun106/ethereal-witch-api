e580f45e0ee2728848c0369cf61891c4bbfdaa6e
stun106 <bladingnordeste@gmail.com>
2024-01-14 12:51:11 -0300

    foi removido o recurso que filtra de forma ilike nomes de categoria.

--- witch/src/main/java/com/ethereal/witch/repository/ICategoryRepository.java
+++ witch/src/main/java/com/ethereal/witch/repository/ICategoryRepository.java
@@ -1,26 +1,22 @@
-package com.ethereal.witch.interfaces;
+package com.ethereal.witch.repository;
 
 import com.ethereal.witch.models.collection.Category;
-import com.ethereal.witch.models.product.Product;
-import com.ethereal.witch.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
+
 @Repository
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
-    Category findByCategoryid(Long idCategory);
-
+    Optional<Category> findByCategoryid(Long idCategory);
+    Category findByNomecategory(String category);
     @Query("SELECT c.nomecategory FROM Category c WHERE c.categoryid = :categoryid")
     Object[] findByid(@Param("categoryid") Long id);
 
-    @Query("SELECT c.nomecategory FROM Category c WHERE LOWER(c.nomecategory) LIKE LOWER(CONCAT('%', :nomecategory ,'%'))")
-    List<Object[]> findBynamecategoyIlike(@Param("nomecategory") String nomecategory);
-
     @Query("SELECT c.nomecategory " + "FROM Category c")
-    List<Object[]> findAllNameCategory();
+    List<Category> findAllNameCategory();
 }
 
