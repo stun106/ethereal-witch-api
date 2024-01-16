8c8ee1cc4f1c005ee694aa4fa350f9ed883d55b2
stun106 <bladingnordeste@gmail.com>
2024-01-16 01:31:04 -0300

    Fix: Recurso CartService implementado

---
+++ witch/src/main/java/com/ethereal/witch/service/CartService.java
@@ -0,0 +1,36 @@
+package com.ethereal.witch.service;
+
+import com.ethereal.witch.models.shoppingcart.CartShopping;
+import com.ethereal.witch.models.user.User;
+import com.ethereal.witch.repository.ICartShoppingRepository;
+import com.ethereal.witch.repository.IUserRepository;
+import com.ethereal.witch.service.exception.EntityNotfoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final ICartShoppingRepository iCartShoppingRepository;
+    private final IUserRepository iUserRepository;
+    @Transactional
+    public CartShopping saveCart(CartShopping cart){
+        return iCartShoppingRepository.save(cart);
+    }
+    @Transactional(readOnly = true)
+    public CartShopping findByCartid(Long id){
+        return iCartShoppingRepository.findById(id).orElseThrow(() ->
+                new EntityNotfoundException(String.format("Cart id {%d} not found", id)));
+    }
+    @Transactional(readOnly = true)
+    public List<Object> findCartInfoByUser(HttpServletRequest request){
+        User user = iUserRepository.findByid((Long) request.getAttribute("idUser"));
+        return  iCartShoppingRepository.findCartShoppingInfoByUsername(user.getId());
+    }
+    @Transactional
+    public void destroyCart(Long id){
+        iCartShoppingRepository.delete(findByCartid(id));
+    }
+}
